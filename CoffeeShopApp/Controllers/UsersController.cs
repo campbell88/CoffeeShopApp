9a83aabe1009bfac938d9fd996e8f46cc8efa9e8
campbell88 <shatharcampbell@gmail.com>
2017-05-10 09:28:17 -0400

    initial commit

---
+++ CoffeeShopApp/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeShopApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult UserRegistration()
+        {
+           
+            return View();
+        }
+        public ActionResult AddUser(string firstName)
+        {
+            
+            ViewBag.FirstName = firstName;
+            return View();
+
+        }
+    }
+}
